405717a32fecb6495c8ef5ed8bd251a630c6d8b4
Nikhil Kaniyeri <69250757+kaniyeri@users.noreply.github.com>
2023-03-20 01:15:26 +0530

    minor changes

--- dpll.py
+++ dpll.py
@@ -4,10 +4,10 @@
 -3,-4,0
 This is the format for clauses.
 '''
-def extract_clauses():
+def extract_clauses(file):
     """Extract the clauses from the file."""
     clauses = []
-    with open('sat.txt') as f:
+    with open(file) as f:
         for line in f:
             clause = line.split(',')
             clause = [int(x) for x in clause]
@@ -17,7 +17,8 @@ def extract_clauses():
 def unit_propagate(clauses, unit):
     """Propagate the unit clause and remove it from the list of clauses."""
 
-
+def main():
+    print(extract_clauses('sat.txt'))
 
 
 if __name__ == '__main__':
