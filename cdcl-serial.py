84d2ad0c32d0260e8244b866609359b7e692e72c
a-saraf <a-saraf@github.com>
2023-03-20 01:59:58 +0530

    cdcl update

--- cdcl-serial.py
+++ cdcl-serial.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class CDCL:
     def __init__(self, formula, no_variables, no_clauses):
         self.formula = formula
@@ -5,16 +7,22 @@ class CDCL:
         self.clauses = []
         self.no_variables = no_variables
         self.no_clauses = no_clauses
+        self.queue = deque()
 
         for clause in formula:
-            self.clauses.append(set(clause))
+            self.clauses.append(list(set(clause)))
 
     def unit_propogation(self):
         unit_clauses  =[clause for clause in self.clauses if len(clause) == 1]
-        if unit_clauses:
+        if len(unit_clauses):
             unit_clause = unit_clauses[0]
             literal = unit_clause[0]
             self.decide(literal)
-
+        else:
+            return
+        
     def decide(self, literal):
         self.assignments[literal] = True
+        self.queue.append(literal)
+
+
