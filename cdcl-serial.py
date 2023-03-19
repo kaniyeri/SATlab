a6b4bf1057f43796b0c67a6df5f73672966e7a72
a-saraf <a-saraf@github.com>
2023-03-20 01:21:08 +0530

    update cdcl

--- cdcl-serial.py
+++ cdcl-serial.py
@@ -1,19 +1,20 @@
 class CDCL:
-    def __init__(self, formula):
+    def __init__(self, formula, no_variables, no_clauses):
         self.formula = formula
         self.assignments = {}
         self.clauses = []
-
+        self.no_variables = self.no_variables
+        self.no_clauses = self.no_clauses
+        
         for clause in formula:
             self.clauses.append(set(clause))
 
     def unit_propogation(self):
         unit_clauses  =[clause for clause in self.clauses if len(clause) == 1]
-        if not unit_clauses:
-            break
-        unit_clause = unit_clauses[0]
-        literal = unit_clause[0]
-        self.decide(literal)
+        if unit_clauses:
+            unit_clause = unit_clauses[0]
+            literal = unit_clause[0]
+            self.decide(literal)
 
     def decide(self, literal):
         self.assignments[literal] = True
